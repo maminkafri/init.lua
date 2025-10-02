fcb810e271722241f0e9a065c08f17167f6ede85
abuzuhair04 <aminkafri0045@gmail.com>
2025-10-02 20:06:44 +0800

    remove fidget

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -95,7 +95,7 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 
 function NoBackground()
     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     -- vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
     -- vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
 end
